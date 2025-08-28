50ca97f4a621fd8a39bd1a4365eb6f8e147b56c7
Edo619 <edo619.coding@gmail.com>
2025-08-28 16:30:40 +0200

    test2

--- randomQuotes.js
+++ randomQuotes.js
@@ -2,17 +2,12 @@ const scrollText = document.getElementById('random-quote');
 
 scrollText.addEventListener('animationiteration', () => {
   // Change text on each loop
-  const quotes = [
-    "Made by Edo619 - 2025",
-    "Welcome to my page",
-    "Random footer message"
-  ];
-  const randomLine = quotes[Math.floor(Math.random() * quotes.length)];
-  scrollText.textContent = randomLine;
   fetch("quotes.json")
   .then(response => response.json())
   .then(quotes => {
     let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
     console.log(`${randomQuote.quote} — ${randomQuote.movie}`);
   });
+  
+  scrollText.textContent = (`${randomQuote.quote} — ${randomQuote.movie}`);
 });
